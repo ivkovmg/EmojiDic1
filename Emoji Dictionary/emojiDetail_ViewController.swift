28efcfbf6bc7b3fd12bfd920bd63cda1a18e61c3
Matthew Ivkovich <ivkovmg@auburn.edu>
2017-08-13 20:29:52 -0400

    First table view is complete

--- Emoji Dictionary/emojiDetail_ViewController.swift
+++ Emoji Dictionary/emojiDetail_ViewController.swift
@@ -12,12 +12,31 @@ class emojiDetail_ViewController: UIViewController {
 
     var emoji = "NO EMOJI"
     
+    @IBOutlet weak var defLabel: UILabel!
     @IBOutlet weak var emojiLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-            emojiLabel.text = emoji
         // Do any additional setup after loading the view.
+        emojiLabel.text = emoji
+        if emoji == "😀"{
+            defLabel.text = "This man be smiling"
+        } else if emoji == "💩"{
+            defLabel.text = "This is crap!"
+        }else if emoji == "😂"{
+            defLabel.text = "That's so funny i can'y stop laughing!!!"
+        }else if emoji == "😺"{
+            defLabel.text = "Cool cats are hard to come by!"
+        }else if emoji == "🤡"{
+            defLabel.text = "Not all clowns are scary."
+        }else if emoji == "😎"{
+            defLabel.text = "Are yo too cool for school?"
+        }else if emoji == "🤑"{
+            defLabel.text = "Money, money, money, money.....Monnneyyyy!"
+        }else {
+            defLabel.text = "There's somthing missing....your face."
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
