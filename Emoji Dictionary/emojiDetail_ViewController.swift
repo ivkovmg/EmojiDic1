af11617ccbea6dadb093c628c1e66d878a4e966a
Matthew Ivkovich <ivkovmg@auburn.edu>
2017-08-13 20:06:58 -0400

    Prepare for segue

---
+++ Emoji Dictionary/emojiDetail_ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  emojiDetail_ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Matthew Ivkovich on 8/13/17.
+//  Copyright © 2017 Mivko Tech. All rights reserved.
+//
+
+import UIKit
+
+class emojiDetail_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+}
